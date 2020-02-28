07ac58709cea4ea8916c0f12757eee83da5fb212
Brent Wallace <brent@bdw.dev>
2020-02-27 21:11:38 -0500

    Installed template engine and set up router in separate file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+const router = require('../routers/routes');
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Carefield Pleasanton');
-});
+app.use(router);
 
-const port = process.env.PORT || 3000;
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
