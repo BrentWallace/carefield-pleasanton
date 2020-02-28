4e52086999a528a5ab9f877eaec830e4b0c105a6
Brent Wallace <brent@bdw.dev>
2020-02-28 12:58:13 -0500

    Reconfigured templates to use partials to include css and scripts

--- routers/routes.js
+++ routers/routes.js
@@ -5,7 +5,12 @@ const router = new express.Router();
 router.get('/', (req, res) => {
   res.render('index.hbs', {
     title: 'Carefield Pleasanton',
-    
+  });
+});
+
+router.get('/photos', (req, res) => {
+  res.render('photos.hbs', {
+    title: 'Photo Gallery',
   });
 });
 
