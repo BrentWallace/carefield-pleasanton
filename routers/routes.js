8be252d26d49125dfa0088b2b4007230ec89f522
Brent Wallace <brent@bdw.dev>
2020-02-28 12:51:26 -0500

    Homepage working. Header and footer partials created.

--- routers/routes.js
+++ routers/routes.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index.hbs');
+  res.render('index.hbs', {
+    title: 'Carefield Pleasanton',
+    
+  });
 });
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -6,15 +6,16 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(router);
-
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+app.use(router);
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
-
 
 const port = process.env.PORT;
 
